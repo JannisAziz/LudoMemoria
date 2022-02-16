58cd36e2d1974e8ddf4f6130052f37a317eca837
Jannis Aziz Khammas <aziz_khammas@yahoo.de>
2022-02-16 02:27:55 +0100

    Security: implemented authentication & UserRole check in UserController

--- backend/src/main/java/de/jannisaziz/backend/security/AuthConfig.java
+++ backend/src/main/java/de/jannisaziz/backend/security/AuthConfig.java
@@ -4,7 +4,6 @@ import de.jannisaziz.backend.user.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,32 +23,17 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //auth.authenticationProvider(daoAuthenticationProvider());
         auth.userDetailsService(userService);
     }
-/*
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(argon2PasswordEncoder);
-        provider.setUserDetailsService(userService);
-        return provider;
-    }
- */
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers("/api/users/**").authenticated()
+                .antMatchers("/api/games/**").permitAll()
                 .antMatchers("/api/auth/**").permitAll()
-                .anyRequest().permitAll()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-                //.and().formLogin();
-
-//                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and().addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
